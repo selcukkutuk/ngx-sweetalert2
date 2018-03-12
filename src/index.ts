aa68a8903a1e981712da062695cb23b5c36bc897
Selçuk Kütük <selcukkutuk@sdu.edu.tr>
2018-03-12 14:46:11 +0300

    fixed options type

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,56 @@
-import { Injectable } from "@angular/core";
-import swal from 'sweetalert2';
+import { Injectable } from '@angular/core'
+import swal, { SweetAlertOptions } from 'sweetalert2'
 
 @Injectable()
 export class SweetAlertService {
-  constructor() { }
-  swal(args = {}) {
-    return swal(args);
+  constructor() {}
+  swal(args: SweetAlertOptions) {
+    return swal(args)
   }
 
   prompt(options) {
     const baseOptions = {
       showCancelButton: true,
-      confirmButtonText: "Submit",
-      input: "text"
-    };
-    return this.swal((<any>Object).assign({}, baseOptions, options));
+      confirmButtonText: 'Submit',
+      input: 'text'
+    }
+    return this.swal((<any>Object).assign({}, baseOptions, options))
   }
 
   confirm(options) {
     const baseOptions = {
       showCancelButton: true,
-      confirmButtonText: "Confirm",
-      type: "warning"
-    };
-    return this.swal((<any>Object).assign(baseOptions, options));
+      confirmButtonText: 'Confirm',
+      type: 'warning'
+    }
+    return this.swal((<any>Object).assign(baseOptions, options))
   }
 
   alert(options) {
     const baseOptions = {
-      confirmButtonText: "OK",
-      type: "info"
-    };
-    return this.swal((<any>Object).assign(baseOptions, options));
+      confirmButtonText: 'OK',
+      type: 'info'
+    }
+    return this.swal((<any>Object).assign(baseOptions, options))
   }
 
   question(options) {
-    return this.alert((<any>Object).assign({ type: "question" }, options));
+    return this.alert((<any>Object).assign({ type: 'question' }, options))
   }
 
   success(options) {
-    return this.alert((<any>Object).assign({ type: "success" }, options));
+    return this.alert((<any>Object).assign({ type: 'success' }, options))
   }
 
   error(options) {
-    return this.alert((<any>Object).assign({ type: "error" }, options));
+    return this.alert((<any>Object).assign({ type: 'error' }, options))
   }
 
   warning(options) {
-    return this.alert((<any>Object).assign({ type: "warning" }, options));
+    return this.alert((<any>Object).assign({ type: 'warning' }, options))
   }
 
   info(options) {
-    return this.alert((<any>Object).assign({ type: "info" }, options));
+    return this.alert((<any>Object).assign({ type: 'info' }, options))
   }
 }
